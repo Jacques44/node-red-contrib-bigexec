4ca2aa975aefd408b51ef391ae4d23930b8dc4b1
Jacques WERNERT <jacques.wernert@i-bp.fr>
2016-05-09 15:15:55 +0200

    stderr on ouput #3 Support for env variables

--- bigexec/bigexec.js
+++ bigexec/bigexec.js
@@ -45,7 +45,7 @@ module.exports = function(RED) {
       "minWarning": 1,                                                // The min return code the node will consider it is a warning
       "minError": 8,                                                  // The min return code the node will consider it is as an error
       "cwd": "",                                                      // The working directory
-      "env": [],                // not implemented yet
+      "env": {},                
       "shell": false,                                                 // Use a shell
       "noStdin": false                                                // Command does require an input (used to avoid EPIPE error)
     }    
@@ -65,11 +65,15 @@ module.exports = function(RED) {
           shell: my_config.shell
         }
 
+        console.log(spawn_config);
+
         // This dummy writable is used when the command does not need any data on its stdin
         // If any data is coming, this stream drops it and no "EPIPE error" is thrown
         var dummy = biglib.dummy_writable(my_config.noStdin);  
 
         this.working("Executing " + my_config.command.substr(0,20) + "...");
+
+        console.log(my_config.commandArgs.concat(my_config.commandArgs2||[]).concat(my_config.commandArgs3||[]));
 
         // Here it is, the job is starting now
         var child = new require('child_process').spawn(my_config.command, my_config.commandArgs.concat(my_config.commandArgs2||[]).concat(my_config.commandArgs3||[]), spawn_config);
@@ -129,6 +133,11 @@ module.exports = function(RED) {
         if (config.extraArgumentProperty && msg[config.extraArgumentProperty]) {
           msg.config = msg.config || {};
           msg.config.commandArgs3 = msg[config.extraArgumentProperty];
+        }
+
+        if (config.envProperty && msg[config.envProperty]) {
+          msg.config = msg.config || {};
+          msg.config.env = msg[config.envProperty];
         }
 
         bignode.main.call(bignode, msg);
